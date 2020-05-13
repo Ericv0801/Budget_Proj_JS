7e9e299d684c46b3f72f449321294dd4afb6f921
Eric Valdivia <ericv0801@gmail.com>
2020-05-13 13:59:50 -0400

    structure in app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+//BUDGET CONTROLLER
+var budgetController = (function(){
+   
+//somcode
+
+})();
+
+//UI CONTROLLER
+var UIController= (function(){
+
+    //some code
+
+})();
+
+
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl,UICtrl){
+    
+
+
+})(budgetController,UIController);
