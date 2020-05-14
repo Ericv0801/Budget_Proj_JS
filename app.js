8abc9854093238fa7b2a0f61813e8ead3c9bf9a6
Eric Valdivia <ericv0801@gmail.com>
2020-05-14 13:52:11 -0400

    added to UI Controller

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 //BUDGET CONTROLLER
-var budgetController = (function(){
-   
-//somcode
-
+var budgetController = (function() {
+  //somcode
 })();
+
+
 
 //UI CONTROLLER
-var UIController= (function(){
-
-    //some code
-
+var UIController = (function() {
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: ".add__btn"
+    }
+  return {
+    getinput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value, // will be description
+        value: document.querySelector(DOMstrings.inputValue).value // will be the amount $$
+      };
+    },
+    getDOMstrings: function(){
+        return DOMstrings;
+    }
+  };
 })();
+
 
 
 //GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl,UICtrl){
+var controller = (function(budgetCtrl, UICtrl) {
 
-    var ctrlAddItem = function(){
+    var DOM = UICtrl.getDOMstrings();
 
+  var ctrlAddItem = function() {
     //1. Get the filled input data
-
+        var input = UICtrl.getinput();
+        console.log(input)
     //2. Add the item to the budget controller
 
     //3. Add the item to the UI
@@ -27,24 +45,12 @@ var controller = (function(budgetCtrl,UICtrl){
     //4. Calculate the  budget
 
     //5. Display the budget on the UI
-    console.log("works")
+  };
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
     }
-document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-document.addEventListener('keypress', function(event) {
-
-    if(event.keyCode === 13 || event.which === 13) {
-        ctrlAddItem();
-    }
-
-});
-
-})(budgetController,UIController);
-
-
-
-
-
-
-
-
+  });
+})(budgetController, UIController);
