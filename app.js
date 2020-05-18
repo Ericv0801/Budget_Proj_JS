1567664bcefc5cd631f11899a8c0b54daf22e93e
Eric Valdivia <ericv0801@gmail.com>
2020-05-18 13:28:38 -0400

    cleared Fields

--- app.js
+++ app.js
@@ -85,6 +85,18 @@ var UIController = (function() {
         document.querySelector(element).insertAdjacentHTML('beforeend', newHtml)
 
     },
+
+    clearFields: function(){
+        var fields, fieldsArr;
+
+        fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+        fieldsArr = Array.prototype.slice.call(fields);
+        fieldsArr.forEach(function(current, index, array){
+            current.value = "";
+        });
+        fieldsArr[0].focus();
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -115,9 +127,13 @@ var controller = (function(budgetCtrl, UICtrl) {
      newItem = budgetCtrl.addItem(input.type, input.description, input.value)
     //3. Add the item to the UI
         UIController.addListItem(newItem, input.type);
-    //4. Calculate the  budget
 
-    //5. Display the budget on the UI
+    //4. Clear The Fiels
+    UICtrl.clearFields();
+
+    //5. Calculate the  budget
+
+    //6. Display the budget on the UI
   };
 
   return {
