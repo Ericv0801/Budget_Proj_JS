93b2c919d530e9f2908b93ed791a1c27d2460a45
Eric Valdivia <ericv0801@gmail.com>
2020-05-26 13:40:23 -0400

    added % to UI

--- app.js
+++ app.js
@@ -129,7 +129,8 @@ var UIController = (function() {
     incomeLabel: ".budget__income--value",
     expensesLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
-    container: ".container"
+    container: ".container",
+    expensesPercLabel: ".item__percentage",
   };
   return {
     getinput: function() {
@@ -192,6 +193,30 @@ var UIController = (function() {
       }
     },
 
+    displayPercentages: function (percentages){
+
+        var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+        var nodeListForEach = function(list,callback){
+            for (var i = 0; i < list.length; i++){
+                callback(list[i], i);
+            }
+        }
+
+        nodeListForEach(fields, function(current, index){
+
+            if(percentages[index]>0){
+            current.textContent = percentages[index] +'%';
+            } else {
+                current.textContent = '--';
+            }
+
+        })
+
+
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -232,7 +257,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 2. Read Percentages from the budget controller.
     var percentages = budgetCtrl.getPercentages();
     // 3. Update the UI with the new percentages
-        console.log(percentages)
+        UICtrl.displayPercentages(percentages);
   };
   var ctrlAddItem = function() {
     var input, newItem;
